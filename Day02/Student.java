b76cb4d8b8a6997fbaa8b6ebbf650cfdf545ca36
Akshara Dixit <dixitakshara96@gmail.com>
2026-07-29 21:33:56 +0530

    Successfully Added Day02

---
+++ Day02/Student.java
@@ -0,0 +1,14 @@
+package Day02;
+
+public class Student {
+    public static void main(String[] args){
+        String name="Rahul";
+        int age=20;
+        String course="B.Tech AI";
+
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Course: "+course);
+    }
+    
+}
