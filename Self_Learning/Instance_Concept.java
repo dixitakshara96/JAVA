c0c54d967ecc200a8d72127b2846cc9bdfd4cf39
Akshara Dixit <dixitakshara96@gmail.com>
2026-08-19 21:17:39 +0530

    Instance method can access Static variable

---
+++ Self_Learning/Instance_Concept.java
@@ -0,0 +1,11 @@
+package Self_Learning;
+
+public class Instance_Concept {
+
+    static int x = 3 ;
+
+    public void main(String []args) {
+        System.out.println("Value of Static variable: "+ x);
+    }
+    
+}
