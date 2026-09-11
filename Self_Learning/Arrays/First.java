fad19f03eda7c40001cbf0b01ebb377b7eaa011f
Akshara Dixit <dixitakshara96@gmail.com>
2026-09-11 23:49:54 +0530

    Arrays in Java

---
+++ Self_Learning/Arrays/First.java
@@ -0,0 +1,38 @@
+package Arrays;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class First {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // collection of Datatypes
+        
+        int[] array = new int[5];
+
+        int[] array1 = {23, 16, 52, 43, 69};
+        
+        System.out.println(array[0]);
+
+        // LHS is the reference variable (stack)
+        // RHS is the object (heap)
+        // Null is a special value literal for reference variable point to in memory for Non Primitives
+
+        int[] array3;
+        array3 = new int[6];
+
+        for (int i = 0 ; i < array3.length ; i++) {
+            System.out.println("Enter element: ");
+            array3[i] = sc.nextInt();
+        }
+
+        for (int element : array3) {
+            System.out.print(element + " ");
+        }
+
+        System.out.println("\n"+ Arrays.toString(array1));
+    }
+    
+}
